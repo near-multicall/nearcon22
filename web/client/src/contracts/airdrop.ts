ad282c38dfc1661879bf21416a9c705605178965
lennart Czardybon <lennczar@gmail.com>
2022-09-14 04:08:53 +0200

    fix: comment out airdrop.ts

--- web/client/src/contracts/airdrop.ts
+++ web/client/src/contracts/airdrop.ts
@@ -1,48 +1,48 @@
-// chluff goes here
-import { view } from "../../src/utils/wallet";
+// // chluff goes here
+// import { view } from "../../src/utils/wallet";
 
-const FACTORY_ADDRESS_SELECTOR: Record<string, string> = {
-    mainnet: "TODO",
-    testnet: "dev-1663082207724-65204983285265",
-};
+// const FACTORY_ADDRESS_SELECTOR: Record<string, string> = {
+//     mainnet: "TODO",
+//     testnet: "dev-1663082207724-65204983285265",
+// };
 
-class NoCapDrop {
-    static CONTRACT_ADDRESS: string = FACTORY_ADDRESS_SELECTOR[window.NEAR_ENV];
-
-
-    static async getNumberAirdrops(): Promise<number> {
-        return view(
-            NoCapDrop.CONTRACT_ADDRESS,
-            "get_number_airdrops",
-            {}
-        );
-    }
-
-    static async getAirdropInfo(airdropId: number): Promise<AirdropInfo> {
-        return view(
-            NoCapDrop.CONTRACT_ADDRESS,
-            "get_airdrop_info",
-            { airdrop_id: airdropId }
-        ); 
-    }
+// class NoCapDrop {
+//     static CONTRACT_ADDRESS: string = FACTORY_ADDRESS_SELECTOR[window.NEAR_ENV];
 
 
+//     static async getNumberAirdrops(): Promise<number> {
+//         return view(
+//             NoCapDrop.CONTRACT_ADDRESS,
+//             "get_number_airdrops",
+//             {}
+//         );
+//     }
 
-    static async getBalance(AccountId: string): Promise<[]>
+//     static async getAirdropInfo(airdropId: number): Promise<AirdropInfo> {
+//         return view(
+//             NoCapDrop.CONTRACT_ADDRESS,
+//             "get_airdrop_info",
+//             { airdrop_id: airdropId }
+//         ); 
+//     }
 
 
-}
+
+//     static async getBalance(AccountId: string): Promise<[]> 
 
 
-type AirdropInfo = {
-    description: string,
-    creator: string,
-    token_id: string,
-    ipfs_cid: string,
-    root_hash: string,
-    expiry_date: number,
-    claimed_users: HashSet<AccountId>,
-    amount: string // u128 encoded as string
-}
+// }
 
-export { NoCapDrop };
+
+// type AirdropInfo = {
+//     description: string,
+//     creator: string,
+//     token_id: string,
+//     ipfs_cid: string,
+//     root_hash: string,
+//     expiry_date: number,
+//     claimed_users: HashSet<AccountId>,
+//     amount: string // u128 encoded as string
+// }
+
+// export { NoCapDrop };
